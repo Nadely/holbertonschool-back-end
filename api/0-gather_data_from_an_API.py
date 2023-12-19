47c64f331f6e94387b6cddce2cd9f630d42368b8
Nadely <6890@holbertonstudents.com>
2023-12-19 09:05:20 +0100

    correction all tasks

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,20 +9,15 @@ from sys import argv
 
 def get_employee_todo_progress():
     user_id = argv[1]
-
     url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-
     response = requests.get(url)
 
-    # Check if the request was successful (status code 200)
     if response.status_code == 200:
-        # Parse the JSON response
         user = response.json()
 
         user_name = user["name"]
 
-
-    url_todos = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id)
+    url_todos = f'https://jsonplaceholder.typicode.com/todos?userId={user_id}'
 
     response_todos = requests.get(url_todos)
     todos = response_todos.json()
@@ -40,6 +35,7 @@ def get_employee_todo_progress():
 
     print("Employee {} is done with tasks ({}/{}):\n\t {}"
           .format(user_name, done_tasks, total_tasks, space_sting))
+
 
 if __name__ == '__main__':
     get_employee_todo_progress()
--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -3,18 +3,15 @@
 returns information about his/her TODO list progress"""
 
 
-import requests
 import json
+import requests
 
 
 def get_employee_todo_progress():
     url = 'https://jsonplaceholder.typicode.com/users'
-
     response = requests.get(url)
 
-    # Check if the request was successful (status code 200)
     if response.status_code == 200:
-        # Parse the JSON response
         users = response.json()
 
         todos_data = {}
@@ -23,10 +20,13 @@ def get_employee_todo_progress():
             user_id = user["id"]
             username = user["username"]
 
-            url_todos = 'https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id)
+            url_todos = (
+    f'https://jsonplaceholder.typicode.com/todos?userId='
+    f'{user_id}'
+)
+
             todos_response = requests.get(url_todos)
 
-            # Check if the request for todos was successful
             if todos_response.status_code == 200:
                 todos = todos_response.json()
 
@@ -37,6 +37,7 @@ def get_employee_todo_progress():
     json_variable = 'todo_all_employees.json'
     with open(json_variable, 'w', newline='') as jsonfile:
         json.dump(todos_data, jsonfile)
+
 
 if __name__ == '__main__':
     get_employee_todo_progress()
