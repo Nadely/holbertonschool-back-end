1ef9778ef436afab8d996e2d873fc693dc4c1d2e
Nadely <6890@holbertonstudents.com>
2023-12-20 12:08:01 +0100

    update gather file

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -2,8 +2,8 @@
 """Write a Python script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress"""
 
-import requests
 from sys import argv
+import requests
 
 
 def get_employee_todo_progress(user_id):
