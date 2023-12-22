4c653d4ae2ca41dea7aef26e09fe0985cff8cf21
Nadely <6890@holbertonstudents.com>
2023-12-22 10:52:25 +0100

    remove task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,38 +9,29 @@ from sys import argv
 def get_employee_todo_progress():
     """Fetch and print TODO list progress for a given user_id."""
 
-    try:
-        user_id = argv[1]
-        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-        response = requests.get(url)
+    user_id = argv[1]
+    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    response = requests.get(url)
 
-        if response.status_code == 200:
-            user = response.json()
-            user_name = user["name"]
+    if response.status_code == 200:
+        user = response.json()
+        user_name = user["name"]
 
-        url_todos = (
-            f'https://jsonplaceholder.typicode.com/todos?userId={user_id}')
+    url_todos = (
+        f'https://jsonplaceholder.typicode.com/todos?userId={user_id}')
 
-        response_todos = requests.get(url_todos)
-        todos = response_todos.json()
+    response_todos = requests.get(url_todos)
+    todos = response_todos.json()
 
-        total_tasks = len(todos)
-        done_tasks = sum(1 for todo in todos if todo['completed'])
+    total_tasks = len(todos)
+    done_tasks = sum(1 for todo in todos if todo['completed'])
 
-        print("Employee {} is done with tasks ({}/{}):"
-              .format(user_name, done_tasks, total_tasks))
+    print("Employee {} is done with tasks ({}/{}):"
+          .format(user_name, done_tasks, total_tasks))
 
-        task_titles = [todo['title'] for todo in todos if todo['completed']]
-        for title in task_titles:
-            print("\t {}".format(title))
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        exit(1)
-
-    if len(argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        exit(1)
+    task_titles = [todo['title'] for todo in todos if todo['completed']]
+    for title in task_titles:
+        print("\t {}".format(title))
 
 
 if __name__ == '__main__':
