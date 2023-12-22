64da33032940ef71fa7e75c42df63a7df56c87d0
Nadely <6890@holbertonstudents.com>
2023-12-22 12:37:34 +0100

    update task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -9,29 +9,38 @@ from sys import argv
 def get_employee_todo_progress():
     """Fetch and print TODO list progress for a given user_id."""
 
+    """response = user info"""
     user_id = argv[1]
     url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
     response = requests.get(url)
+    user = response.json()
 
     if response.status_code == 200:
         user = response.json()
         user_name = user["name"]
 
+    """todos = todo info"""
     url_todos = (
         f'https://jsonplaceholder.typicode.com/todos?userId={user_id}')
 
     response_todos = requests.get(url_todos)
     todos = response_todos.json()
 
-    total_tasks = len(todos)
-    task_titles = [todo['title'] for todo in todos if todo['completed']]
-    done_tasks = len(task_titles)
 
-    print("Employee {} is done with tasks ({}/{}):"
-          .format(user_name, done_tasks, total_tasks))
+    task_completed = [task for task in todos if task['completed']]
 
-    for title in task_titles:
-        print("\t {}".format(title))
+
+    user_name = user["name"]
+    len_completed_tasks = len(task_completed)
+    total_todo = len(todos)
+
+    print("Employee {} is done with tasks({}/{}):".format(
+            user_name,
+            len_completed_tasks,
+            total_todo))
+
+    for task in task_completed:
+        print(f"\t {task['title']}")
 
 
 if __name__ == '__main__':
